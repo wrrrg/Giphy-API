1ce98c737550cc5fe5f6bda3268a1b238341e77b
wrrrg <bill.gryta@gmail.com>
2018-02-01 16:15:57 -0600

    all functionality for JS done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,13 +12,78 @@ $(document).ready(function() {
     // clear out the array to avoid repeating all of them
     $("#giphyButtons").empty();
     populateButtons();
+    queryGiphy(newButton);
   });
 
-  $(".giphy-button").on("click", function(){
+  $(document).on("click", '.giphy-button', function(){
     // grab user input
     var giphy = $(this).attr("data");
     console.log(giphy);
+    queryGiphy(giphy);
+
+
     // create search URL for giphy
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    //   giphy + "&api_key=dc6zaTOxFJmzC&limit=10";
+    //
+    // $.ajax({
+    //   url: queryURL,
+    //   method: 'GET'
+    // }).then(function(response) {
+    //   console.log(response);
+    //   // store AJAX request data in a results variable
+    //   var results = response.data;
+    //   // clear previous giphyButtons
+    //   $("#giphys-appear-here").empty();
+    //   // Loop through the results
+    //   for (var i=0; i<results.length; i++) {
+    //     // make the div element
+    //     var giphyDiv = $("<div class='gif-div'>");
+    //     // make the rating text element
+    //     var p = $("<p>").text("Rating: " + results[i].rating);
+    //     // make the gif element
+    //     var giphyImg = $("<img>");
+    //     giphyImg.attr("src", results[i].images.fixed_height.url);
+    //
+    //     // Append paragraph and image to div
+    //     giphyDiv.append(p);
+    //     giphyDiv.append(giphyImg);
+    //
+    //     // prepend the giphyDiv to the main images div
+    //     $("#giphys-appear-here").prepend(giphyDiv);
+    //   };
+    // });
+
+  });
+
+  $(document).on("click", 'img', function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      console.log(this);
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+
+  function populateButtons(){
+    // Loop to create giphy buttons
+      for (var i = 0; i < giphyArr.length; i++) {
+        var giphyButton = $("<button>").addClass('giphy-button');
+        giphyButton = giphyButton.attr('data', giphyArr[i]);
+        giphyButton.text(giphyArr[i]);
+        $("#giphyButtons").prepend(giphyButton);
+      };
+  };
+
+  function queryGiphy(str){
+    var giphy = str;
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       giphy + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -38,8 +103,10 @@ $(document).ready(function() {
         // make the rating text element
         var p = $("<p>").text("Rating: " + results[i].rating);
         // make the gif element
-        var giphyImg = $("<img>");
-        giphyImg.attr("src", results[i].images.fixed_height.url);
+        var giphyImg = $("<img class='giphy'>");
+        giphyImg.attr("src", results[i].images.fixed_height_still.url);
+        giphyImg.attr("data-still", results[i].images.fixed_height_still.url);
+        giphyImg.attr("data-animate", results[i].images.fixed_height.url);
 
         // Append paragraph and image to div
         giphyDiv.append(p);
@@ -49,17 +116,6 @@ $(document).ready(function() {
         $("#giphys-appear-here").prepend(giphyDiv);
       };
     });
-
-  });
-
-  function populateButtons(){
-    // Loop to create giphy buttons
-      for (var i = 0; i < giphyArr.length; i++) {
-        var giphyButton = $("<button>").addClass('giphy-button');
-        giphyButton = giphyButton.attr('data', giphyArr[i]);
-        giphyButton.text(giphyArr[i]);
-        $("#giphyButtons").prepend(giphyButton);
-      };
-  };
+  }
 
 });
